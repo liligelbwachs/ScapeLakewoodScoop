68f5947a2e76e0034fce937826a872479e05d564
Lili Gelbwachs <liligelbwachs@gmail.com>
2019-05-13 21:22:02 -0400

    Add project files.

---
+++ LakewoodScoopScrape/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LakewoodScoopScrape.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
